9539c68733c8890c16c5004a3dc36e49eb78aec0
Duncan Overbruck <overbruck@mir.de>
2014-07-28 19:33:19 +0200

    cleaned generated file

--- generate.py
+++ generate.py
@@ -3,10 +3,7 @@ from django.conf import settings
 
 settings.configure()
 
-head = """
-# completion for django
-
-
+head = """# completion for django
 function __fish_complete_django -d "Completions for django and its aliases" --argument-names cmd
 \techo "load django completions for: $cmd"
 \tcomplete -x -c $cmd
@@ -25,7 +22,7 @@ options = []
 
 for cmd in dj_commands.keys():
     c = load_command_class(dj_commands[cmd], cmd)
-    commands.append('complete -c $cmd -n \'__fish_use_subcommand\' -a %s -d "%s"' % (cmd, c.help))
+    commands.append('complete -c $cmd -n \'__fish_use_subcommand\' -a %s -d "%s"' % (cmd, c.help.replace("\n", "").replace('"', '\\"')))
     for opt in c.option_list:
         if not opt in dj_options:
             dj_options[opt] = []
@@ -34,7 +31,7 @@ for cmd in dj_commands.keys():
 for opt in dj_options.keys():
     s = '-s %s' % opt._short_opts[0][1:] if opt._short_opts else None
     l = '-l %s' % opt._long_opts[0][2:] if opt._long_opts else None
-    d = '-d "%s"' % opt.help if opt.help else None
+    d = '-d "%s"' % opt.help.replace("\n", "").replace('"', '\\"') if opt.help else None
     a = '-a "%s"' % ' '.join(opt.choices) if opt.choices else None
     options.append('complete -c $cmd -n \'__fish_seen_subcommand_from %s\' %s' % (
         ' '.join(dj_options[opt]),
